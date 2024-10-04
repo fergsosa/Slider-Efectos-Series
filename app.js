b4b35ef63423f34714dd19e837016aac74f11175
fergsosa <134444526+fergsosa@users.noreply.github.com>
2024-10-04 14:12:50 -0300

    optimizo codigo

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ const d = document,
   $nextDom = d.getElementById("next"),
   $prevDom = d.getElementById("prev"),
   $carouselDom = d.querySelector(".carousel"),
-  SliderDom = $carouselDom.querySelector(".carousel .list"),
+  $SliderDom = $carouselDom.querySelector(".carousel .list"),
   $thumbnailBorderDom = d.querySelector(".carousel .thumbnail"),
   $thumbnailItemsDom = $thumbnailBorderDom.querySelectorAll(".item");
 
-timeDom = d.querySelector(".carousel .time");
+// $timeDom = d.querySelector(".carousel .time");
 
 $thumbnailBorderDom.appendChild($thumbnailItemsDom[0]);
 
 let timeRunning = 1000;
 let timeAutoNext = 7000;
 
-$nextDom.onclick = () => {
-  showSlider("next");
-};
-
-$prevDom.onclick = () => {
-  showSlider("prev");
-};
+$nextDom.onclick = () => showSlider("next");
+$prevDom.onclick = () => showSlider("prev");
 
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
@@ -28,15 +23,15 @@ let runNextAuto = setTimeout(() => {
 }, timeAutoNext);
 
 function showSlider(type) {
-  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+  let $SliderItemsDom = $SliderDom.querySelectorAll(".carousel .list .item");
   let $thumbnailItemsDom = d.querySelectorAll(".carousel .thumbnail .item");
 
   if (type === "next") {
-    SliderDom.appendChild(SliderItemsDom[0]);
+    $SliderDom.appendChild($SliderItemsDom[0]);
     $thumbnailBorderDom.appendChild($thumbnailItemsDom[0]);
     $carouselDom.classList.add("next");
   } else {
-    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    $SliderDom.prepend($SliderItemsDom[$SliderItemsDom.length - 1]);
     $thumbnailBorderDom.prepend(
       $thumbnailItemsDom[$thumbnailItemsDom.length - 1]
     );
@@ -44,6 +39,7 @@ function showSlider(type) {
   }
 
   clearTimeout(runTimeOut);
+
   runTimeOut = setTimeout(() => {
     $carouselDom.classList.remove("next");
     $carouselDom.classList.remove("prev");
